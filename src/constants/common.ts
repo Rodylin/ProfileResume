b084d1551b4861982205d014016dd68c8da35443
BædBlood⊹ <rodylinbernales339@gmail.com>
2024-09-06 08:40:37 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Rodylin Bernales';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Aspiring software developer currently studying computer science, eager to learn and grow in the field of software development. Enthusiastic about coding, problem-solving, and building applications that make a difference.';
+export const ABOUT_TEXT_2 =
+  'Dedicated to developing skills in both frontend and backend web development. I enjoy working on projects that challenge my abilities and push me to expand my knowledge of programming languages, frameworks, and tools.';
+export const ABOUT_TEXT_3 =
+  'Versatile student proficient in the basics of web development. I am currently building my skills in various programming languages, frameworks, and databases, aiming to become a full-stack developer capable of creating complete web applications.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Computer science student with hands-on experience in building websites and small-scale web applications using modern JavaScript tools and frameworks. I am continuously expanding my knowledge of both front-end and back-end technologies, as well as database management.';
+export const RESUME_TEXT_2 =
+  'Student developer passionate about coding and eager to gain more experience in web development, focusing on learning the latest technologies and best practices in the industry.';
